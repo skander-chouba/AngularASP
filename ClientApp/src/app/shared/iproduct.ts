659b258c66e2ea12ad578d22ce77b8ef420b3a89
Skander Chouba <skander.chouba@gmail.com>
2020-06-03 15:19:02 +0100

    creating the product model

--- ClientApp/src/app/shared/iproduct.ts
+++ ClientApp/src/app/shared/iproduct.ts
@@ -1,2 +1,11 @@
 export interface Iproduct {
+  id: number;
+  title: string;
+  category: string;
+  size: string;
+  price: number;
+  artDescription: string;
+  artDate: Date;
+  artistName: string;
+  artistId: string;
 }
