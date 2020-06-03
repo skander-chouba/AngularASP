ef8f9f18b761083796a723964a9d6385c4a6789d
Skander Chouba <skander.chouba@gmail.com>
2020-06-03 10:38:38 +0100

    Add path to products component

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { ProductsComponent } from './products/products.component';
 
 const routes: Routes = [
 { path: '', component: HomeComponent, pathMatch: 'full' },
 { path: 'counter', component: CounterComponent },
 { path: 'fetch-data', component: FetchDataComponent },
+{ path: 'products', component: ProductsComponent },
 ];
 
 @NgModule({
